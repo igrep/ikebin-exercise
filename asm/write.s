97be48b3156e45aae24b49ed519dc0c984c93cd5
Yamamoto Yuji <whosekiteneverfly@gmail.com>
2013-11-17 17:07:05 +0900

    add notes to the sample assembly sources

--- asm/write.s
+++ asm/write.s
@@ -1,9 +1,10 @@
 ! Assembly for MINIX
 ! write(1, hello, 6);
-mov ax, #1
+
+mov ax, #1 ! 出力先を標準出力に設定
 int 7
-.data1 4
-.data2 hello, 6
+.data1 4 ; sys write
+.data2 hello, 6 ; データセクション helloから6バイト分
 
 ! exit(0);
 mov ax, #0
